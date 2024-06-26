f3e0d504845517faf57a7f203902f59821cdac28
zhangmin <zm.123>
2024-06-26 15:24:45 +0800

    111

--- main.ts
+++ main.ts
@@ -1,19 +1,24 @@
-//% weight=0 color=#3CB371 icon="\uf0ad" block="Tools"
-namespace tools {
+/**
+ * namespace定义积木的选单package
+ */
+//% weight=0 color=#3CB371 icon="\uf0ad" block="百度 AI Tools"
+namespace baiduAi {
+
+    let api_key = ""
+    let api_secret = ""
     /**
-    * 計算長方形面積，並回傳
+    * 初始化 api_key api_secret
+    * blockId可自定，通常就用函式名称即可。
+     * weight同样是定义积木出现的顺序，在这里是数字愈小愈上面。
+     * blockGap定义积木和积木之间的间距
+     * block则定义了积木中要出现的字，以及对应的输入参数名称，参数名称前面要加%，每个参数中间必须要加上|以做为区隔
+     * blockExternalInputs=true则定义了二个参数以上的输入，以竖排排的方式排版积木
     */
-    //% blockId="areaOfRectangle" block="area of rectangle length %length|width %width"
+    //% blockId="serApiConfig" block="设置Api key %apiKey| api secret %apiSecret"
     //% blockGap=2 weight=0 blockExternalInputs=true
-    export function areaOfRectangle(length: number, width:number): number {
-        return length*width
+    export function serApiConfig(apiKey: string, apiSecret:string): void {
+        api_key = apiKey
+        api_secret = apiSecret
     }
-    /**
-    * 計算長方形面積，不回傳，只顯示在LED
-    */
-    //% blockId="ledOfRectangle" block="show area of rectangle length %length|width %width"
-    //% blockGap=2 weight=1
-    export function ledOfRectangle(length: number, width:number): void {
-        basic.showNumber(length*width)
-    }
+
 }
